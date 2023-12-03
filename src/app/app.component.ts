25dc6a5140a9c2ab643a0737e82c020e1f8b1490
Jesús Guijarro <72225659+JesusEduardoGuijarroSaldana@users.noreply.github.com>
2023-12-03 12:59:30 -0600

    exercise ended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { EventBindingComponent } from './event-binding/event-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, EventBindingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'CBDF_JEGS_11';
